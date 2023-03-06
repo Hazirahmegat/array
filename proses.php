13c50cec6f9ce19ed980c9ea92f8873a07e75c49
Nur Hazirah <hzrhmgt@gmail.com>
2023-03-06 15:47:55 +0800

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,12 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    sarah
-</body>
-</html>
+<?php
+
+$berat = $_POST['berat'];
+$tinggi = $_POST['tinggi'];
+$bmi = $berat / ($tinggi ** 2);
+
+$userdata = array(
+    'berat' => $berat,
+    'tinggi' => $tinggi,
+    'bmi' => $bmi
+);
+
+if($bmi < 18.5){
+    $hasil = "Kurung berat badan";
+}else if($bmi <= 24.9){
+    $hasil = "Normal";
+}else if($bmi <= 29.9){
+    $hasil = "Lebih berat badan";
+}else{
+    $hasil = "Obesiti";
+}
+?>
