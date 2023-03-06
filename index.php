7f3a5a48eda1cd3be60654653642f9c0fb5b00ca
Sarah Qadija <saerah06@gmail.com>
2023-03-06 15:11:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,28 @@
     <title>Document</title>
 </head>
 <body>
-    sarah
+<form action="proses.php" method="post">
+        <table>
+
+            <tr>
+                <td><label for="name">NAMA:</label></td>
+                <td><input type="text" name="nama" id="nama" require></td>
+            </tr>
+
+            <tr>
+                <td><label for="bulan">BULAN:</label></td>
+                <td><input type="number" name="bulan" id="bulan" require></td>
+            </tr>
+
+            <tr>
+                <td><label for="harga">HARGA:</label></td>
+                <td><input type="number" name="harga" id="harga" require></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="submit" value="Submit"></td>
+                <td><input type="reset" name="reset" value="Reset"></td>
+            </tr>
+        </table>
+    </form>
 </body>
 </html>
